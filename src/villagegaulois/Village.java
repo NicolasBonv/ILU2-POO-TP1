198ad96acde505d5d7acbf8974d0bb54cf8e7fe4
Nicolas Bonvalet <bnn3786a@ensfsi.onmicrosoft.com>
2024-02-28 10:40:27 +0100

    Fin de la premiere feuille -> utilisation du main

--- src/villagegaulois/Village.java
+++ src/villagegaulois/Village.java
@@ -7,11 +7,13 @@ public class Village {
 	private String nom;
 	private Chef chef;
 	private Gaulois[] villageois;
+	private Marche marche;;
 	private int nbVillageois = 0;
 
-	public Village(String nom, int nbVillageoisMaximum) {
+	public Village(String nom, int nbVillageoisMaximum, int nbEtals) {
 		this.nom = nom;
 		villageois = new Gaulois[nbVillageoisMaximum];
+		this.marche = new Marche(nbEtals);
 	}
 
 	public String getNom() {
@@ -21,10 +23,62 @@ public class Village {
 	public void setChef(Chef chef) {
 		this.chef = chef;
 	}
+	// DEBUT CLASSE INTERNE //
 	public class Marche{
-		private Etal etals[] = new Etal[50];
+		int nbEtals; 
+		private Etal etals[] = new Etal[nbEtals];
+		
+		public Marche(int nbEtals) {
+			this.nbEtals = nbEtals;
+			for(int i=0;i<nbEtals;i++) {
+				etals[i] = null;
+			}
+		}
+		void utiliserEtal(int indEtal, Gaulois vendeur, String produit, int nbProd) {
+			etals[indEtal].occuperEtal(vendeur, produit, nbProd);
+		}
+		int trouverEtalLibre() {
+			for(int i = 0; i < nbEtals;i++) {
+				if (etals[i].isEtalOccupe()) {
+					return i;
+				}	
+			}
+			return -1;
+		}
+		Etal[] trouverEtals(String prod) {
+			Etal etalsProd[] = new Etal[nbEtals];
+			int j = 0;
+			for(int i = 0; i < nbEtals;i++) {
+				if(etals[i].contientProduit(prod)) {
+					etalsProd[j] = etals[i];
+					j++;
+				}
+			}
+			return etalsProd;
+		}
+		Etal trouverVendeur(Gaulois gaulois) {
+			for(int i = 0; i < nbEtals;i++) {
+				if(etals[i].getVendeur() == gaulois) {
+					return etals[i];
+				}
+			}
+			return null;
+		}
+		void afficherMarche() {
+			int nbEtalVide = 0 ;
+			for(int i = 0; i<nbEtals;i++) {
+				if(etals[i] != null) {
+					etals[i].afficherEtal();
+				}else {
+					nbEtalVide++;
+				}
+			}
+			if (nbEtalVide != 0) {
+				System.out.println("Il reste "+ nbEtalVide +" étals non utilisés dans le marché");
+			}
+		}
 	}
-
+	// FIN CLASSE INTERNE //
 	public void ajouterHabitant(Gaulois gaulois) {
 		if (nbVillageois < villageois.length) {
 			villageois[nbVillageois] = gaulois;
